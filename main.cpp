5e2b0a8c5aca78e206d8187b91570b1536d255fe
Ahmad Zuhdi <ahmad.zuhdi.y@gmail.com>
2017-12-05 20:26:19 +0700

    update sleketon

--- main.cpp
+++ main.cpp
@@ -7,26 +7,96 @@
 #include <tr1/functional>
 
 // declare all needed functions
-int showLogin();
 int setupFile();
+int showLogin(bool clearScreen);
+int showMainMenu();
 bool checkFileExists(char path[255]);
+unsigned long generateHash(const char *str);
 
 // declare all struct
 struct userData {
     char username[50];
-    int hashPassword;
+    unsigned long hashPassword;
     char role[50];
 };
+
+// TODO: update this struct
+struct transactionData {
+};
+
+int currentSupportedUsers = 15;
+
+userData users[15]; // TODO: find a better way to dynamically set this struct length
+transactionData transactions[1000000]; // TODO: update this, currently only support up to 1M data
 
 using namespace std;
 
 int main() {
+
+    cout << "Aplikasi TBD" << endl;
+
     setupFile();
-    showLogin();
+    showLogin(true);
 };
 
-int showLogin() {
-    clrscr();
+int showLogin(bool clearScreen) {
+
+
+    if (clearScreen) {
+        //clrscr(); // TODO: enable this later
+    }
+
+    char endln[2];
+    strcpy(endln, "\n");
+
+    char inputedUsername[50];
+
+    cout << "Username: "; gets(inputedUsername);
+    cout << "Password: ";
+
+    string password;
+    char inputKey;
+
+    inputKey = _getch();
+
+    while(inputKey != 13) { // looping selama bukan enter yang di pencet
+        password.push_back(inputKey);
+        cout << "*";
+        inputKey = _getch();
+    }
+
+    int matchedUser = -1;
+
+    for(int i = 0; i < currentSupportedUsers; i++) {
+
+        char userName[50];
+        strcpy(userName, users[i].username);
+        int userPassword = users[i].hashPassword;
+
+        cout << inputedUsername << ":" << userName;
+
+        bool isUsernameMatch = (strcmp(inputedUsername, userName) == 0);
+
+        cout << isUsernameMatch << endl;
+
+        if (isUsernameMatch && (userPassword == generateHash(password.c_str()))) {
+            matchedUser = i;
+            i = currentSupportedUsers;
+        }
+    }
+
+    if (matchedUser >= 0) { // user found
+        showMainMenu();
+        return 1;
+    } else {
+        cout << endln << "Username " << inputedUsername << " not found" << endl;
+        return showLogin(false);
+    }
+}
+
+int showMainMenu() {
+    //clrsrc();
+    cout << "\t\tWelcome";
 }
 
 // setup initial needed file
@@ -42,21 +112,29 @@ int setupFile() {
     if (!isUsersFileExists) {
 
         // check file users.txt udah ada atau belum
-        // kalo engga, bikin file baru users.txt yang bentuk nya csv = username, password, role
+        // kalo engga, bikin file baru users.txt yang bentuk nya ssv (space separated value) = username password role
 
         ofstream userFile;
         userFile.open(usersFileName);
 
-        // generate hash password buat admin
-        int adminPassword = tr1::hash<string>()("admin");
-
         // tulis baris pertama buat user admin
-        userFile << "admin,";
-        userFile << adminPassword;
-        userFile << ",admin";
+        userFile << "admin ";
+        userFile << generateHash("admin");
+        userFile << " admin";
 
         // bikin file users.txt
         userFile.close();
+    } else {
+        // kalo file nya udah ada, kita looping, masukin ke struct users;
+
+        ifstream input(usersFileName);
+        string line;
+        int i=0;
+
+        while(getline(input, line)) {
+            sscanf(line.c_str(), "%s %i %s", users[i].username, &users[i].hashPassword, users[i].role);
+            i++;
+        }
     }
 
     if (!isTransactionsFileExists) {
@@ -66,6 +144,15 @@ int setupFile() {
         transactionFile.open(transactionsFileName);
 
         transactionFile.close();
+    } else {
+        ifstream input(transactionsFileName);
+        string line;
+        int i = 0;
+
+        while(getline(input, line)) {
+            //sscanf(line.c_str(), "%s %i %s", users[i].username, &users[i].hashPassword, users[i].role);
+            i++;
+        }
     }
 
     return 1;
@@ -78,4 +165,15 @@ bool checkFileExists(char path[255]) {
     } else {
         return false;
     }
+}
+
+unsigned long generateHash(const char *str)
+{
+    unsigned long hash = 5381;
+    int c;
+
+    while (c = *str++)
+        hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+
+    return hash;
 }
