390198ebd04b4009cc2fce05bb3ad320ff6a6e93
AhmadZuhdi <ahmad.zuhdi.y@gmail.com>
2017-12-13 16:55:56 +0700

    finishing up, it should already meet all recruitment

--- main-simple.cpp
+++ main-simple.cpp
@@ -17,6 +17,7 @@ void setupBases();
 int showLogin();
 int showMainMenu();
 int showAddTransaction();
+int showAllTransactions();
 int showSetting();
 string showInputPassword();
 void clearScr();
@@ -45,10 +46,13 @@ struct transactionData {
     int userId;
     int baseId;
     int toppingsId[5]; // up to 5 toppings
+    float totalPrice;
+    float totalPayment;
 };
 
 int currentSupportedUsers = 10;
 int maxToppings = 5;
+int latestTransactionIndex = 0;
 
 int loginUserIndex = -1;
 userData loggedUser;
@@ -57,6 +61,7 @@ userData users[10]; // TODO: find a better way to dynamically set this struct le
 topping toppings[50]; // TODO: find a better way to dynamically set this struct length
 base bases[50]; // TODO: find a better way to dynamically set this struct length
 transactionData transactions[1000000]; // TODO: update this, currently only support up to 1M data
+
 
 int main() {
 
@@ -108,8 +113,8 @@ void setupBases() {
 
 void setupTopping() {
     // coklat
-    toppings[0].price = 1000;
-    strcpy(toppings[0].name, "Coklat");
+    toppings[5].price = 1000;
+    strcpy(toppings[5].name, "Coklat");
 
     // susu
     toppings[1].price = 500;
@@ -189,6 +194,8 @@ int showMainMenu() {
     switch(selectedMenu) {
         case 1:
             return showAddTransaction();
+        case 2:
+            return showAllTransactions();
         case 3:
             return showSetting();
         default:
@@ -212,20 +219,112 @@ int showAddTransaction() {
     // kita cuma ambil base yang harga nya di atas 0 aja
     for (int i = 0; i < totalBases; i++) {
         if (bases[i].price > 0) {
-            totalSelectableBases++;n
-            //cout <<
+            totalSelectableBases++;
+            cout << (i+1) << ". " << bases[i].name << " (Rp. " << bases[i].price << ")" << endl;
         }
     }
 
     int selectedBaseKey=0;
+    int userToppings = 0;
+    int selectedUserToppings[5];
+    float totalHarga = 0;
+    transactionData trx;
 
-    cout << endl << "Masukkan no pancong [0-" << totalSelectableBases << "] : ";
+    cout << endl << "Masukkan kode pancong [1-" << (totalSelectableBases) << "] : ";
+    cin >> selectedBaseKey;
+    selectedBaseKey--;
+
+    totalHarga = totalHarga + bases[selectedBaseKey].price;
+
+    cout << endl;
+    cout << endl << "Jumlah Topping yang di inginkan (max 5): ";
+    cin >> userToppings;
+
+    if (userToppings < 1) {
+        userToppings = 1;
+    }
+
+    if (userToppings > 5) {
+        userToppings = 5;
+    }
+
+    cout << endl << "Pilihan Topping: " << endl;
 
     for (int a = 0; a < totalToppings; a++) {
-        if (toppings[a].price > 0) {
+        topping selectedTopping = toppings[a];
+        if (selectedTopping.price > 0) {
             totalSelectableToppings++;
+            cout << (a) << ". " << selectedTopping.name << " (Rp. " << selectedTopping.price << ")" << endl;
         }
     }
+
+    cout << endl;
+
+    for(int b = 0; b < userToppings; b++) {
+        int selectedTopping = 0;
+        cout << "Masukkan kode topping [1-" << totalSelectableToppings << "]: ";
+        cin >> selectedTopping;
+
+        totalHarga = totalHarga + toppings[selectedTopping].price;
+        trx.toppingsId[b] = selectedTopping;
+    }
+
+    float totalBayar;
+
+    totalHarga = totalHarga + (totalHarga * 0.1);
+
+    cout << endl << "Total Harga (Termasuk PPN 10%) : Rp." << totalHarga;
+
+    trx.baseId = selectedBaseKey;
+    //trx.toppingsId = selectedUserToppings;
+    trx.totalPrice = totalHarga;
+    trx.userId = loginUserIndex;
+
+    cout << endl << "Total Bayar : ";
+    cin >> trx.totalPayment;
+
+    transactions[latestTransactionIndex] = trx;
+    latestTransactionIndex++;
+
+    cout << endl << "Transaksi berhasil di buat!"; getch();
+    showMainMenu();
+}
+
+int showAllTransactions() {
+    clearScr();
+
+    cout << "\t\tTransaksi Sesi ini";
+    cout << endl << "================================================";
+    cout << endl << endl;
+
+    printf("%-4s%-10s%-25s%-15s%-15s", "#", "Kasir", "Tipe Pancong", "Topping", "Total Harga");
+
+    for(int i = 0; i < latestTransactionIndex; i++) {
+        transactionData trx = transactions[i];
+        topping firstTopping = toppings[trx.toppingsId[0]];
+
+        cout << endl;
+        printf("%-4d%-10s%-25s%-15s", (i+1), users[trx.userId].username, bases[trx.baseId].name, firstTopping.name);
+        int totalTopping = 1;
+        for(int a = 1; a < maxToppings-1; a++) {
+            topping selectedTopping = toppings[trx.toppingsId[a]];
+
+            if (trx.toppingsId[a] > 0) {
+                totalTopping++;
+                cout << endl;
+                printf("%-4s%-10s%-25s%-15s", "", "", "", selectedTopping.name);
+            }
+        }
+
+        topping lastTopping = toppings[trx.toppingsId[totalTopping]];
+
+        cout << endl;
+        printf("%-4s%-10s%-25s%-15s%-15.0f", "", "", "", lastTopping.name, trx.totalPrice);
+        cout << endl;
+    }
+
+    cout << "Tekan tombol apapun untuk kembali ke menu utama"; getch();
+    showMainMenu();
 }
 
 int showSetting() {
