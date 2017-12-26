00ad34d28c41350e839ade2c42958b3c2c2af4be
Ahmad Zuhdi <ahmad.zuhdi.y@gmail.com>
2017-12-26 14:55:01 +0700

    Create arduino_jarak.c

---
+++ arduino_jarak.c
@@ -0,0 +1,25 @@
+#define triggerPin 13
+#define echoPin 12
+
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(triggerPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(triggerPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(triggerPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(triggerPin, LOW);
+  long duration, distance;
+  duration = pulseIn(echoPin, HIGH);
+  distance = (duration/2);
+  Serial.print("duration: "); Serial.print(distance / 29.1); Serial.print("\n");
+  Serial.print("distance: "); Serial.print(distance); Serial.print("\n");
+  delay(500);
+}
